f26a84263a57993213da0f904cf646c8be4c1ce2
coderkamlesh <coderkamlesh68@gmail.com>
2024-01-23 12:31:11 +0530

    first commit

---
+++ 8-set-method/script.js
@@ -0,0 +1,18 @@
+$(document).ready(function () {
+    // $("#clickbutton").click(function () {
+
+    //     //set text method
+    //     $("#box h2").text("Hello coderShot")
+    //     // $("#box p").text("Hello This is new text")
+
+    //     //set html method
+    //     $("#box p").html("Hello <b>This is new text</b>")
+
+    //     //set attr method
+    //     $("#box h2").attr("class", "red")
+    // })
+
+    //set val method
+    $("#sname").val("kamlesh");
+    $("#sclass").val(12)
+})
